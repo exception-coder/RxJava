7d0f8ed0eb0fb81ef3eb08d495f9aa2d944cc334
zhangkai <425485346@qq.com>
2019-09-10 00:35:01 +0800

    RxJava

--- rxjava/src/main/java/cn/exceptioncode/api/ObservableApiTest.java
+++ rxjava/src/main/java/cn/exceptioncode/api/ObservableApiTest.java
@@ -1,16 +1,23 @@
 package cn.exceptioncode.api;
 
 import io.reactivex.Observable;
+import lombok.SneakyThrows;
 import org.junit.Test;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class ObservableApiTest {
 
     private final String[] monthArray = {"Jan", "Feb", "Mar", "Apl", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
 
+    private final Integer[] numbers = {1, 2, 13, 34, 15, 17};
+
+    private final String[] fruits = {"苹果", "梨", "李子", "荔枝", "芒果"};
+
+
     @Test
-    public void tets() {
+    public void test() {
         Observable<String> observable = Observable.fromArray(monthArray);
         observable = observable.map(String::toUpperCase);
         observable.subscribe(System.out::println);
@@ -37,9 +44,7 @@ public class ObservableApiTest {
 
 
     /**
-     *
      * 分组操作
-     *
      */
     @Test
     public void groupByTest() {
@@ -48,20 +53,62 @@ public class ObservableApiTest {
             System.out.println("***************************************************************************************");
             System.out.println(observable);
             observable.subscribe(item ->
-                    System.out.println(Thread.currentThread().getName() + ":" + observable.getKey()+":"+item)
+                    System.out.println(Thread.currentThread().getName() + ":" + observable.getKey() + ":" + item)
             );
         });
     }
 
     /**
-     *
      * 数据源合并操作
-     *
      */
     @Test
-    public void mergTest(){
-        Observable.merge(Observable.range(1,5),Observable.range(10,3))
-                .subscribe(item -> System.out.println("we got: "+ item+" from the Observable"),
-                        throwable -> System.out.println(throwable.getMessage()),() -> System.out.println("Emission completed"));
+    public void mergTest() {
+        Observable.merge(Observable.range(1, 5), Observable.range(10, 3))
+                .subscribe(item -> System.out.println("we got: " + item + " from the Observable"),
+                        throwable -> System.out.println(throwable.getMessage()), () -> System.out.println("Emission completed"));
     }
+
+
+    /**
+     * zip合并操作 获取不到元素则会跳过处理
+     */
+    @Test
+    public void zipTest() {
+        Observable.zip(Observable.fromArray(numbers), Observable.fromArray(fruits), (obj1, obj2) -> obj1 + "个" + obj2).subscribe(item -> {
+            System.out.println(item);
+        });
+    }
+
+    @SneakyThrows
+    @Test
+    public void combineLatestTest() {
+        Observable.combineLatest(Observable.fromArray(numbers), Observable.fromArray(fruits), (obj1, obj2) -> obj1 + "个" + obj2).subscribe(item -> {
+            System.out.println(item);
+        });
+
+        Observable.combineLatest(Observable.interval(2, TimeUnit.SECONDS).map(x -> "Java" + x),
+                Observable.interval(1, TimeUnit.SECONDS).map(x -> "Spring" + x),
+                (obj1, obj2) -> obj1 + ":" + obj2).subscribe(item -> {
+            System.out.println(item);
+        });
+
+        Thread.sleep(6 * 1000);
+    }
+
+    @SneakyThrows
+    @Test
+    public void withLatestFromTest() {
+        Observable.fromArray(numbers).withLatestFrom(Observable.fromArray(fruits), (item1, item2) -> item1 + "个" + item2).forEach(System.out::println);
+        System.out.println("******************************************************************************************");
+        Observable observable1 = Observable.interval(2, TimeUnit.SECONDS).map(x -> "Java" + x);
+        Observable observable2 = Observable.interval(1, TimeUnit.SECONDS).map(x -> "Spring" + x);
+        observable1.withLatestFrom(observable2, (s, f) -> s + ":" + f).forEach(System.out::println);
+        Thread.sleep(6 * 1000);
+    }
+
+    @Test
+    public void ambWithTest() {
+    }
+
+
 }
